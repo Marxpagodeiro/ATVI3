5000484639f05e30ee624d3cba0123cba6f0354e
Marxpagodeiro <113648003+Marxpagodeiro@users.noreply.github.com>
2022-10-31 21:55:07 -0300

    Add files via upload

---
+++ LetraG.java
@@ -0,0 +1,169 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package letrag;
+
+/**
+ *
+ * @author 20222TPMI0035
+ */
+public class LetraG {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        char[][] letraG = {{'P','P','P','P',' ',},
+                              {'P',' ',' ',' ','P',},
+                              {'P','P','P','P',' '},
+                              {'P',' ',' ',' ',' '},
+                              {'P',' ',' ',' ',' '}
+                             };
+        System.out.println(letraG[0]);
+        System.out.println(letraG[1]);
+        System.out.println(letraG[2]);
+        System.out.println(letraG[3]);
+        System.out.println(letraG[4]);
+    }
+    public static char[][] letraG(char letra){
+        if(letra == 'A'){
+           char[][] letraG = {{' ',' ','A',' ',' ',},
+                              {' ','A',' ','A',' ',},
+                              {'A','A','A','A','A'},
+                              {'A',' ',' ',' ','A'},
+                              {'A',' ',' ',' ','A'}
+                              };
+        }
+        if(letra == 'B'){
+           char[][] letraG = {{'B','B','B','B','B',},
+                              {'B',' ',' ',' ','B',},
+                              {'B','B','B','B','B'},
+                              {'B',' ',' ',' ','B'},
+                              {'B','B','B','B','B'}
+                             };
+        }
+        if(letra == 'C'){
+           char[][] letraG = {{'C','C','C','C','C',},
+                              {'C',' ',' ',' ',' ',},
+                              {'C',' ',' ',' ',' '},
+                              {'C',' ',' ',' ',' '},
+                              {'C','C','C','C','C'}
+                             };
+        }
+        if(letra == 'D'){
+           char[][] letraG = {{'D','D','D','D','D',},
+                              {'D',' ',' ',' ','D',},
+                              {'D',' ',' ',' ','D'},
+                              {'D',' ',' ',' ','D'},
+                              {'D','D','D','D','D'}
+                             };
+        }
+        if(letra == 'E'){
+           char[][] letraG = {{'E','E','E','E','E',},
+                              {'E',' ',' ',' ',' ',},
+                              {'E','E','E','E','E'},
+                              {'E',' ',' ',' ',' '},
+                              {'E','E','E','E','E'}
+                             };
+        }
+        if(letra == 'F'){
+           char[][] letraG = {{'F','F','F','F','F',},
+                              {'F',' ',' ',' ',' ',},
+                              {'F','F','F','F',' '},
+                              {'F',' ',' ',' ',' '},
+                              {'F',' ',' ',' ',' '}
+                             };
+        }
+        if(letra == 'G'){
+           char[][] letraG = {{'G','G','G','G','G',},
+                              {'G',' ',' ',' ',' ',},
+                              {'G',' ',' ','G','G'},
+                              {'G',' ',' ',' ','G'},
+                              {'G','G','G','G','G'}
+                             };
+        }
+        if(letra == 'H'){
+           char[][] letraG = {{'H',' ',' ',' ','H',},
+                              {'H',' ',' ',' ','H',},
+                              {'H','H','H','H','H'},
+                              {'H',' ',' ',' ','H'},
+                              {'H',' ',' ',' ','H'}
+                             };
+        }
+        if(letra == 'I'){
+           char[][] letraG = {{' ','I','I','I',' ',},
+                              {' ',' ','I',' ',' ',},
+                              {' ',' ','I',' ',' '},
+                              {' ',' ','I',' ',' '},
+                              {' ','I','I','I',' '}
+                             };
+        }
+        if(letra == 'J'){
+           char[][] letraG = {{' ',' ','J','J','J',},
+                              {' ',' ',' ','J',' ',},
+                              {' ',' ',' ','J',' '},
+                              {'J',' ',' ','J',' '},
+                              {'J','J','J','J',' '}
+                             };
+        }
+        if(letra == 'K'){
+           char[][] letraG = {{'K',' ',' ',' ','K',},
+                              {'K',' ',' ','K',' ',},
+                              {'K','K','K',' ',' '},
+                              {'K',' ',' ','K',' '},
+                              {'K',' ',' ',' ','K'}
+                             };
+        }
+        if(letra == 'L'){
+           char[][] letraG = {{'L',' ',' ',' ',' ',},
+                              {'L',' ',' ',' ',' ',},
+                              {'L',' ',' ',' ',' '},
+                              {'L',' ',' ',' ',' '},
+                              {'L','L','L','L','L'}
+                             };
+        }
+        if(letra == 'M'){
+           char[][] letraG = {{'M','M',' ','M','M',},
+                              {'M','M','M','M','M',},
+                              {'M',' ','M',' ','M'},
+                              {'M',' ',' ',' ','M'},
+                              {'M',' ',' ',' ','M'}
+                             };
+        }
+        if(letra == 'N'){
+           char[][] letraG = {{'N',' ',' ',' ','N',},
+                              {'N','N',' ',' ','N',},
+                              {'N',' ','N',' ','N'},
+                              {'N',' ',' ','N','N'},
+                              {'N',' ',' ',' ','N'}
+                             };
+        }
+        if(letra == 'O'){
+           char[][] letraG = {{' ','O','O','O',' ',},
+                              {'O',' ',' ',' ','O',},
+                              {'O',' ',' ',' ','O'},
+                              {'O',' ',' ',' ','O'},
+                              {' ','O','O','O',' '}
+                             };
+        }
+        if(letra == 'P'){
+           char[][] letraG = {{'P','P','P','P',' ',},
+                              {'P',' ',' ',' ','P',},
+                              {'P','P','P','P',' '},
+                              {'P',' ',' ',' ',' '},
+                              {'P',' ',' ',' ',' '}
+                             };
+        }
+        
+        
+        
+        return letraG;
+        
+    }
+    
+    
+}
+    
+
