5fbc5b0463b03e5ab5571071735475c9cacbf0ae
Marxpagodeiro <113648003+Marxpagodeiro@users.noreply.github.com>
2022-11-01 19:11:05 -0300

    Add files via upload

--- LetraG.java
+++ LetraG.java
@@ -15,11 +15,11 @@ public class LetraG {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        char[][] letraG = {{'P','P','P','P',' ',},
-                              {'P',' ',' ',' ','P',},
-                              {'P','P','P','P',' '},
-                              {'P',' ',' ',' ',' '},
-                              {'P',' ',' ',' ',' '}
+        char[][] letraG = {{'D','D','D','D',' ',},
+                              {'D',' ',' ',' ','D',},
+                              {'D',' ',' ',' ','D'},
+                              {'D',' ',' ',' ','D'},
+                              {'D','D','D','D',' '}
                              };
         System.out.println(letraG[0]);
         System.out.println(letraG[1]);
@@ -27,143 +27,260 @@ public class LetraG {
         System.out.println(letraG[3]);
         System.out.println(letraG[4]);
     }
-    public static char[][] letraG(char letra){
-        if(letra == 'A'){
-           char[][] letraG = {{' ',' ','A',' ',' ',},
+    public static char[][] letraG(char[] letra){
+        char [][] resp = new char[5][5];
+        for(int i=0;i<letra.length;i++){
+        if(letra[i] == 'A'){
+           char[][]letraG = {{' ',' ','A',' ',' ',},
                               {' ','A',' ','A',' ',},
                               {'A','A','A','A','A'},
                               {'A',' ',' ',' ','A'},
                               {'A',' ',' ',' ','A'}
                               };
+           resp = letraG;
         }
-        if(letra == 'B'){
-           char[][] letraG = {{'B','B','B','B','B',},
+        if(letra[i] == 'B'){
+           char[][] letraG = {{'B','B','B','B',' ',},
                               {'B',' ',' ',' ','B',},
                               {'B','B','B','B','B'},
                               {'B',' ',' ',' ','B'},
-                              {'B','B','B','B','B'}
+                              {'B','B','B','B',' '}
                              };
+           resp = letraG;
         }
-        if(letra == 'C'){
+        if(letra[i] == 'C'){
            char[][] letraG = {{'C','C','C','C','C',},
                               {'C',' ',' ',' ',' ',},
                               {'C',' ',' ',' ',' '},
                               {'C',' ',' ',' ',' '},
                               {'C','C','C','C','C'}
                              };
+           resp = letraG;
         }
-        if(letra == 'D'){
-           char[][] letraG = {{'D','D','D','D','D',},
+        if(letra[i] == 'D'){
+           char[][] letraG = {{'D','D','D','D',' ',},
                               {'D',' ',' ',' ','D',},
                               {'D',' ',' ',' ','D'},
                               {'D',' ',' ',' ','D'},
-                              {'D','D','D','D','D'}
+                              {'D','D','D','D',' '}
                              };
+           resp = letraG;
         }
-        if(letra == 'E'){
+        if(letra[i] == 'E'){
            char[][] letraG = {{'E','E','E','E','E',},
                               {'E',' ',' ',' ',' ',},
                               {'E','E','E','E','E'},
                               {'E',' ',' ',' ',' '},
                               {'E','E','E','E','E'}
                              };
+           resp = letraG;
         }
-        if(letra == 'F'){
+        if(letra[i] == 'F'){
            char[][] letraG = {{'F','F','F','F','F',},
                               {'F',' ',' ',' ',' ',},
                               {'F','F','F','F',' '},
                               {'F',' ',' ',' ',' '},
                               {'F',' ',' ',' ',' '}
                              };
+           resp = letraG;
         }
-        if(letra == 'G'){
+        if(letra[i] == 'G'){
            char[][] letraG = {{'G','G','G','G','G',},
                               {'G',' ',' ',' ',' ',},
                               {'G',' ',' ','G','G'},
                               {'G',' ',' ',' ','G'},
                               {'G','G','G','G','G'}
                              };
+           resp = letraG;
         }
-        if(letra == 'H'){
+        if(letra[i] == 'H'){
            char[][] letraG = {{'H',' ',' ',' ','H',},
                               {'H',' ',' ',' ','H',},
                               {'H','H','H','H','H'},
                               {'H',' ',' ',' ','H'},
                               {'H',' ',' ',' ','H'}
                              };
+           resp = letraG;
         }
-        if(letra == 'I'){
+        if(letra[i] == 'I'){
            char[][] letraG = {{' ','I','I','I',' ',},
                               {' ',' ','I',' ',' ',},
                               {' ',' ','I',' ',' '},
                               {' ',' ','I',' ',' '},
                               {' ','I','I','I',' '}
                              };
+           resp = letraG;
         }
-        if(letra == 'J'){
+        if(letra[i] == 'J'){
            char[][] letraG = {{' ',' ','J','J','J',},
                               {' ',' ',' ','J',' ',},
                               {' ',' ',' ','J',' '},
                               {'J',' ',' ','J',' '},
                               {'J','J','J','J',' '}
                              };
+           resp = letraG;
         }
-        if(letra == 'K'){
+        if(letra[i] == 'K'){
            char[][] letraG = {{'K',' ',' ',' ','K',},
                               {'K',' ',' ','K',' ',},
                               {'K','K','K',' ',' '},
                               {'K',' ',' ','K',' '},
                               {'K',' ',' ',' ','K'}
                              };
+           resp = letraG;
         }
-        if(letra == 'L'){
+        if(letra[i] == 'L'){
            char[][] letraG = {{'L',' ',' ',' ',' ',},
                               {'L',' ',' ',' ',' ',},
                               {'L',' ',' ',' ',' '},
                               {'L',' ',' ',' ',' '},
                               {'L','L','L','L','L'}
                              };
+           resp = letraG;
         }
-        if(letra == 'M'){
+        if(letra[i] == 'M'){
            char[][] letraG = {{'M','M',' ','M','M',},
                               {'M','M','M','M','M',},
                               {'M',' ','M',' ','M'},
                               {'M',' ',' ',' ','M'},
                               {'M',' ',' ',' ','M'}
                              };
+           resp = letraG;
         }
-        if(letra == 'N'){
+        if(letra[i] == 'N'){
            char[][] letraG = {{'N',' ',' ',' ','N',},
                               {'N','N',' ',' ','N',},
                               {'N',' ','N',' ','N'},
                               {'N',' ',' ','N','N'},
                               {'N',' ',' ',' ','N'}
                              };
+           resp = letraG;
         }
-        if(letra == 'O'){
+        if(letra[i] == 'O'){
            char[][] letraG = {{' ','O','O','O',' ',},
                               {'O',' ',' ',' ','O',},
                               {'O',' ',' ',' ','O'},
                               {'O',' ',' ',' ','O'},
                               {' ','O','O','O',' '}
                              };
+           resp = letraG;
         }
-        if(letra == 'P'){
+        if(letra[i] == 'P'){
            char[][] letraG = {{'P','P','P','P',' ',},
                               {'P',' ',' ',' ','P',},
                               {'P','P','P','P',' '},
                               {'P',' ',' ',' ',' '},
                               {'P',' ',' ',' ',' '}
                              };
+           resp = letraG;
         }
         
+        if (letra[i] == 'Q'){
+           char[][] letraG = {{'Q','Q','Q','Q','Q',},
+                              {'Q',' ',' ',' ','Q' ,},
+                              {'Q',' ',' ',' ','Q'},
+                              {'Q','Q','Q','Q','Q'},
+                              {' ',' ',' ',' ','Q'}
+                             };
+            resp = letraG;                 
+        }
+        if (letra[i] == 'R'){
+           char[][] letraG = {{'R','R','R','R',' ',},
+                              {'R',' ',' ',' ','R',},
+                              {'R','R','R','R',' '},
+                              {'R','R',' ',' ',' '},
+                              {'R',' ','R',' ',' '}
+                              };
+        resp = letraG;
+       
+        }
+        if(letra[i] == 'S'){
+           char[][] letraG = {{'S','S','S','S','S',},
+                              {'S',' ',' ',' ' ,' ',},
+                              {'S','S','S','S','S'},
+                              {' ',' ',' ',' ','S'},
+                              {'S','S','S','S','S'}
+                              };
+        resp = letraG;   
+        }
+        if (letra[i] == 'T'){
+           char[][] letraG = {{'T','T','T','T','T',},
+                              {' ',' ','T',' ',' ',},
+                              {' ',' ','T',' ',' '},
+                              {' ',' ','T',' ',' '},
+                              {' ',' ','T',' ',' '}
+                              };
+           resp = letraG;
+        }   
+        if(letra[i] == 'U'){
+           char[][] letraG = {{'U',' ',' ',' ','U',},
+                              {'U',' ',' ',' ','U',},
+                              {'U',' ',' ',' ','U'},
+                              {'U',' ',' ',' ','U'},
+                              {'U','U','U','U','U'}
+                             };
+           resp = letraG;
+           
+        }
+         if(letra[i] == 'V'){
+           char[][] letraG = {{'V',' ',' ',' ','V',},
+                              {'V',' ',' ',' ','V',},
+                              {' ','V',' ','V',' '},
+                              {' ',' ','V',' ',' '},
+                              {' ',' ','V',' ',' '}
+                             };
+           resp = letraG;
+         }
+         if(letra[i] == 'W'){
+           char[][] letraG = {{'W',' ',' ',' ','W',},
+                              {'W',' ',' ',' ','W',},
+                              {'W',' ','W',' ','W'},
+                              {'W','W',' ','W','W'},
+                              {'W',' ',' ',' ','W'}
+                             };
+           resp = letraG;
+      
+         }
+         if(letra[i] == 'X'){
+           char[][] letraG = {{'X',' ',' ',' ','X',},
+                              {' ','X',' ','X',' ',},
+                              {' ',' ','X',' ',' '},
+                              {' ','X',' ','X',' '},
+                              {'X',' ',' ',' ','X'}
+                             };
+           
+            resp = letraG;
+         }
+          if(letra[i] == 'Y'){
+           char[][] letraG = {{'Y',' ',' ',' ','Y',},
+                              {' ','Y',' ','Y',' ',},
+                              {' ',' ','Y',' ',' '},
+                              {' ','Y',' ',' ',' '},
+                              {'Y',' ',' ',' ',' '}
+                             };
+           
+            resp = letraG;
+          }
+             if(letra[i] == 'Z'){
+           char[][] letraG = {{'Z','Z','Z','Z','Z',},
+                              {' ',' ',' ','Z',' ',},
+                              {' ',' ','Z',' ',' '},
+                              {' ','Z',' ',' ',' '},
+                              {'Z','Z','Z','Z','Z'}
+                             };
+           resp = letraG;
+        }
+                
+           return resp;
         
-        
-        return letraG;
-        
+ 
+    
+    
     }
+        
     
     
-}
+    
+
     
 
